e8bef41386c4ff556033c19e008547af8e86c060
Chris Wilson <chris+github@qwirx.com>
2014-06-20 14:09:49 +0100

    Use unminified sources as bundle constituents.
    Allows debugging bundled code in debug mode.

--- pyquip/assets.py
+++ pyquip/assets.py
@@ -1,16 +1,19 @@
 from django_assets import Bundle, register
 
 def standard_bundle(name, source):
-    bundle = Bundle('js/jquip/dist/%s' % source,
-        output='js/%s' % source)
+    bundle = Bundle('js/jquip/src/%s.js' % source,
+        filters=['jsmin'],
+        output='js/pyquip/%s.min.js' % source)
     register('jquip.%s' % name, bundle)
     return bundle
 
-all = standard_bundle('all', 'jquip.all.min.js')
-core = standard_bundle('core', 'jquip.min.js')
-ajax = standard_bundle('ajax', 'jquip.ajax.min.js')
-css  = standard_bundle('css', 'jquip.css.min.js')
-custom = standard_bundle('custom', 'jquip.custom.min.js')
-docready = standard_bundle('docready', 'jquip.docready.min.js')
-events = standard_bundle('events', 'jquip.events.min.js')
-q_min = standard_bundle('q_min', 'jquip.q-min.min.js')
+core = standard_bundle('core', 'jquip')
+ajax = standard_bundle('ajax', 'jquip.ajax')
+css  = standard_bundle('css', 'jquip.css')
+custom = standard_bundle('custom', 'jquip.custom')
+docready = standard_bundle('docready', 'jquip.docready')
+events = standard_bundle('events', 'jquip.events')
+q_min = standard_bundle('q_min', 'jquip.q-min')
+
+all = Bundle(core, ajax, css, custom, docready, events, q_min,
+    output='js/pyquip/jquip.all.min.js')
